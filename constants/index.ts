82f6fe1cf2070c0812ad291850a824cb657c738b
Vinojan Veerapathrathasan <imvinojanv@gmail.com>
2024-05-24 16:35:11 +0530

    Constants 🧮

---
+++ constants/index.ts
@@ -0,0 +1,7 @@
+export const units = [
+    { id: 'unit-1', name: 'Unit 1' },
+    { id: 'unit-2', name: 'Unit 2' },
+    { id: 'unit-3', name: 'Unit 3' },
+    { id: 'unit-4', name: 'Unit 4' },
+    { id: 'unit-5', name: 'Unit 5' }
+];
