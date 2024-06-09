99eff201cde35723e98b381248b9138c9a53e328
Vinojan Veerapathrathasan <imvinojanv@gmail.com>
2024-06-09 13:52:46 +0530

    19

--- lib/rfid.ts
+++
@@ -1,6 +0,0 @@
-// const {devicePortBind , cmdDeviceRegistryContinuesTagID , devicePortOpenReadSerialData} = require('./serialService');
-import {devicePortBind , cmdDeviceRegistryContinuesTagID , devicePortOpenReadSerialData} from './serialService';
-
-devicePortBind();
-cmdDeviceRegistryContinuesTagID();
-devicePortOpenReadSerialData();
