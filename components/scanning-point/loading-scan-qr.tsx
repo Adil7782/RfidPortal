7605aa9698883d081678b45082ef6fd5f13a8646
Vinojan Veerapathrathasan <imvinojanv@gmail.com>
2024-06-15 09:52:19 +0530

    QR scanning loading model

---
+++ components/scanning-point/loading-scan-qr.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image"
+
+const LoadingScanQR = () => {
+  return (
+    <div>
+        <Image
+            src='/images/scanning-qr.gif'
+            alt="scanning qr"
+            width={600}
+            height={200} 
+            className="mt-2 w-full rounded-md border"       
+        />
+        <p className="-mt-8 flex items-center justify-center gap-2 text-slate-500">
+            <div className="animate-ping animate-ping-delayed w-3 h-3 bg-[#ea8460] rounded-full"/>
+            Scanning...
+        </p>
+        <p className="mb-11 absolute bottom-0 text-xl text-slate-800 font-medium">Please scan your QR code</p>
+    </div>
+  )
+}
+
+export default LoadingScanQR
