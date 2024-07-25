86e792eeceb951d0da3c9e3966982810675df1c3
Vinojan Veerapathirathasan <imvinojanv@gmail.com>
2024-07-25 11:38:37 +0530

    Updated QC RFID loading

--- app/points/product-assembly-qc/_components/qc-dashboard-panel.tsx
+++ app/points/product-assembly-qc/_components/qc-dashboard-panel.tsx
@@ -58,7 +58,15 @@ const QCDashboardPanel = ({
         setRfidTag(tag);
         try {
             const response = await axios.get(`/api/scanning-point/product?rfid=${tag}`);
-            setQcProduct(response.data.data);
+            if (response.data.data) {
+                setQcProduct(response.data.data);
+            } else {
+                toast({
+                    title: "No data found for this RFID, Please try again!",
+                    variant: "error"
+                });
+                router.refresh();
+            }
         } catch (error: any) {
             toast({
                 title: error.response.data || "Something went wrong",
@@ -159,15 +167,16 @@ const QCDashboardPanel = ({
                                     style={qcProduct.frontGmt.styleNo}
                                 />
                                 :
-                                <div className='w-full min-h-40 bg-slate-100 rounded-lg border flex justify-center items-center'>
-                                    <Loader className='animate-spin text-slate-600' />
-                                </div>
+                                // <div className='w-full min-h-40 bg-slate-100 rounded-lg border flex justify-center items-center'>
+                                //     <Loader className='animate-spin text-slate-600' />
+                                // </div>
+                                <ReadingRFIDDialogModel handleRfidTag={handleRfidTag} />
                             }
                         </>
                     }
 
                     {/* Submit Button */}
-                    {rfidTag &&
+                    {rfidTag && qcProduct &&
                         <QCSubmitDialogModel
                             handleSubmit={handleSubmit}
                             isSubmitting={isSubmitting}
@@ -183,7 +192,7 @@ const QCDashboardPanel = ({
                 </div>
                 <div className='w-2/3 space-y-6'>
                     {/* Defects List */}
-                    {rfidTag ?
+                    {rfidTag && qcProduct ?
                         <QCMultiSelectDefects
                             defects={defects}
                             selectedDefects={selectedDefects}
