ebb7376e5921a018acb1ca42a636059cbf203927
Vinojan Veerapathrathasan <imvinojanv@gmail.com>
2024-08-28 16:53:03 +0530

    test 2

--- app/points/button-section-out/page.tsx
+++ app/points/button-section-out/page.tsx
@@ -1,12 +1,12 @@
 import ManageBulkProductDashboard from "./_components/manage-bulk-products-dashboard";
 
 const ScanningPoint8Page = () => {
-  const rfidPattern = /e28069150000.{12}/g;
   function extractRFIDTags(hexString: string): string[] {
+    const rfidPattern = /e28069950000[\da-f]{12}/ig;
     return [...hexString.matchAll(rfidPattern)].map(match => match[0]);
   }
 
-  const stng = "a55a0019833000e280699500005014cca7398600d301760d0a";
+  const stng = "a55a0019833000e280699500005014cca7398600d301760d0aa55a0019833000e280699500005014cca7355600d301763re3";
 
   const newTags = extractRFIDTags(stng);
   console.log("TAGS", newTags);
