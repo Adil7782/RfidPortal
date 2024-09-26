fb3143d27ed6d7766bfc40bd1de17a1a9ad70405
Vinojan Veerapathrathasan <imvinojanv@gmail.com>
2024-09-26 09:56:48 +0530

    Updated admin dashboard look

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -28,40 +28,36 @@ const AdminLayout = ({
     };
 
     return (
-        <section className='w-full h-screen bg-slate-100'>
-            <div className="flex felx-col h-full md:w-64 fixed inset-y-0 z-50">
+        <div className="h-screen w-full">
+            <div className="flex felx-col h-full w-64 fixed inset-y-0 z-50">
                 <Sidebar />
             </div>
-            <main className="md:pl-64">
-                <section className="h-screen lg:pr-[10px] lg:py-2">
-                    <div className="bg-white overflow-auto pb-4 h-full lg:rounded-xl shadow-sm md:border">
-                        <div className="sticky top-0 py-2 pr-3 pl-6 w-full z-50 border-b rounded-t-lg flex justify-between items-center bg-white/80 backdrop-blur-lg">
-                            <h1 className='text-lg font-medium text-slate-800'>Admin Dashboard</h1>
-                            <UserProfileButton
-                                role={verified?.user.role}
-                                name={verified?.user.name}
-                                email={verified?.user.email}
-                                pointNo={verified?.scanningPoint.pointNo}
-                            />
-                        </div>
-                        <div className="admin-dashboard-body-height p-4">
-                            {children}
-                        </div>
-                        <div className="bottom-0 flex justify-center">
-                            <div className='p-4 flex items-center gap-2'>
-                                <p className='text-sm text-slate-500'>Powered by</p>
-                                <Image
-                                    src='/images/logo.svg'
-                                    alt='Logo Image'
-                                    width={100}
-                                    height={100}
-                                />
-                            </div>
-                        </div>
+            <div className="ml-64 h-full">
+                <div className="sticky top-0 py-2 pr-3 pl-6 w-full z-10 border-b shadow-sm flex justify-between items-center bg-white/80 backdrop-blur-lg">
+                    <h1 className='text-lg font-medium text-slate-800'>Admin Dashboard</h1>
+                    <UserProfileButton
+                        role={verified?.user.role}
+                        name={verified?.user.name}
+                        email={verified?.user.email}
+                        pointNo={verified?.scanningPoint.pointNo}
+                    />
+                </div>
+                <main className="admin-dashboard-body-height px-4">
+                    {children}
+                </main>
+                <div className="bottom-0 flex justify-center">
+                    <div className='p-4 flex items-center gap-2'>
+                        <p className='text-sm text-slate-500'>Powered by</p>
+                        <Image
+                            src='/images/logo.svg'
+                            alt='Logo Image'
+                            width={100}
+                            height={100}
+                        />
                     </div>
-                </section>
-            </main>
-        </section>
+                </div>
+            </div>
+        </div>
     )
 }
 
