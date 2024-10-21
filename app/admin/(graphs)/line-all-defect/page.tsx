b70d656e86a12fceb2f98bdd28943d6a729ab15b
Your Name <adilsaaly@gmail.com>
2024-10-21 17:01:43 +0530

    small fix

--- app/admin/(graphs)/line-all-defect/page.tsx
+++ app/admin/(graphs)/line-all-defect/page.tsx
@@ -1,8 +1,9 @@
 // import SelectObbSheetDateHour from "@/components/dashboard/common/select-obbsheet-date-hour"
 import { db } from "@/lib/db";
-import EfficiencyAnalyticsChart from "./_components/analytics-chart";
-import { getUnit } from "./_components/actions";
+
 import { CardTitle } from "@/components/ui/card";
+import { getUnit } from "./_components/actions";
+import EfficiencyAnalyticsChart from "./_components/analytics-chart";
 
 const AchivementRateoperation = async () => {
     
