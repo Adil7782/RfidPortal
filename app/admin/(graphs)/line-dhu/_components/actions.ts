3eb136ff48829a2f704412cd41c697af0e78cf24
Your Name <adilsaaly@gmail.com>
2024-10-31 12:01:14 +0530

    somewhatok

---
+++ app/admin/(graphs)/line-dhu/_components/actions.ts
@@ -0,0 +1,132 @@
+"use server";
+import { neon } from "@neondatabase/serverless";
+import { ProductionDataType } from "./analytics-chart";
+import { defectData } from "./bar-chart-graph";
+
+type defData  = {
+    defectcount:number,obbid:string
+}
+
+
+export async function getOperatorEfficiency(obbsheetid:string,date:string) : Promise<defectData[]>   {
+    const sql = neon(process.env.DATABASE_URL || "");
+
+    
+     const data = await sql`SELECT 
+   
+    count(pd.*) as count,ler."endQcTarget" as target
+FROM  
+    "ProductDefect" pd
+ inner join "LineEfficiencyResources" ler on ler."obbSheetId" = pd."obbSheetId"
+WHERE 
+    pd."obbSheetId" = ${obbsheetid} 
+    AND pd."timestamp" LIKE ${date}
+    AND pd."qcStatus" ='pass'
+    
+    group by target
+;
+`
+    
+            console.log(data)
+            console.log(date,obbsheetid)
+    
+    
+    return new Promise((resolve) => resolve(data as defectData[] ))
+}
+
+export async function getDefects() : Promise<defData[]>   {
+    const sql = neon(process.env.DATABASE_URL || "");
+    // obbsheetid:string,date:string
+    
+     const data = await sql`select 
+        count(gd.id) as defectcount,
+       "obbSheetId" as obbid
+      from "GmtDefect" gd
+      left join "_GmtQC" gqc ON gqc."B" = gd.id
+      left join "Defect" d ON d.id = gqc."A"
+      where gd."qcStatus" <> 'pass' 
+     
+        and gd.timestamp like '2024-09-10%'
+        group by obbid
+        
+        
+      UNION
+  select 
+        count(pd.id) as defectcount,
+        "obbSheetId" as obbid
+       
+      from "ProductDefect" pd
+      left join "_ProductQC" pqc ON pqc."B" = pd.id
+      left join "Defect" d ON d.id = pqc."A"
+      where pd."qcStatus" <> 'pass' 
+ 
+        and pd.timestamp like '2024-09-10%'
+      group by d.name,obbid
+;
+`
+    
+    
+    
+    return new Promise((resolve) => resolve(data as defData[] ))
+}
+
+
+
+export async function getObb(unit:any) : Promise<{ id: string; name: string }[]>  {
+    const sql = neon(process.env.ELIOT_DATABASE_URL || "");
+
+    
+     const data = await sql`
+    select os.name as name ,os.id as id from "ObbSheet" os 
+
+inner join "Unit" u on u.id= os."unitId"
+
+where os."unitId"=${unit}
+ order by os."createdAt" desc
+
+`
+    return new Promise((resolve) => resolve(data as { id: string; name: string }[]))
+}
+
+export async function getUnits() : Promise<{obbid: any; unit: string;line: string; units:string
+}[]>  {
+    const sql = neon(process.env.ELIOT_DATABASE_URL || "");
+
+    
+     const data = await sql`
+    select u.name as unit,u.id units,pl.name as line,os.id as obbid,os.name as obbsheet from "Unit"u 
+inner join "ProductionLine" pl on pl."unitId" = u.id
+inner join "ObbSheet" os on os."productionLineId" =pl.id
+
+`
+    return new Promise((resolve) => resolve(data as { unit: string;line: string; obbid: string;units :string }[]))
+}
+
+export async function getUnit() : Promise<{ id: string; name: string }[]>  {
+    const sql = neon(process.env.ELIOT_DATABASE_URL || "");
+
+    
+     const data = await sql`
+     select id as id , name as name from "Unit" u 
+
+
+ order by "createdAt" desc
+
+`
+    return new Promise((resolve) => resolve(data as { id: string; name: string }[]))
+}
+
+export async function getLine(obbsheetid:string)   {
+    const sql = neon(process.env.ELIOT_DATABASE_URL || "");
+
+    
+     const data = await sql`
+     select pl."id",pl."name" from "ProductionLine" pl
+inner join "ObbSheet" os on os."productionLineId" =pl."id"
+where os.id = ${obbsheetid}
+
+
+
+`
+    return new Promise((resolve) => resolve(data ))
+}
---
+++ app/admin/(graphs)/line-dhu/page.tsx
@@ -0,0 +1,43 @@
+// import SelectObbSheetDateHour from "@/components/dashboard/common/select-obbsheet-date-hour"
+import { db } from "@/lib/db";
+import EfficiencyAnalyticsChart from "./_components/analytics-chart";
+import { getUnit } from "./_components/actions";
+import { CardTitle } from "@/components/ui/card";
+
+const AchivementRateoperation = async () => {
+    
+const unit = await getUnit() ;
+
+
+// console.log("hello",unit)
+
+
+    return (
+        <div>
+
+
+<div className="sticky top-16 py-2 pr-3 pl-6 w-full z-50 border-b rounded-t-lg  items-center bg-white/80 backdrop-blur-lg">
+
+            <CardTitle className="text-center sticky">
+                {" "}
+                {" "}
+                Line Wise DHU
+              </CardTitle>
+              </div>
+
+
+
+            <EfficiencyAnalyticsChart
+                obbSheets={"obbSheets"}
+                units={unit}
+            />
+            
+        </div>
+
+
+
+
+    )
+}
+
+export default AchivementRateoperation
