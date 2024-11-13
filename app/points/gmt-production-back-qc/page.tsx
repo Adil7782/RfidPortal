74ac8ca8e932fd4c43705be61180a9ea66ed702d
Vinojan Veerapathrathasan <imvinojanv@gmail.com>
2024-11-13 10:48:42 +0530

    Updated select OBB size

--- app/points/gmt-production-back-qc/page.tsx
+++ app/points/gmt-production-back-qc/page.tsx
@@ -11,7 +11,7 @@ const GmtQCScanningPointDashboard = async () => {
     }
 
     return (
-        <div className='mx-auto max-w-2xl'>
+        <div className='mx-auto max-w-4xl'>
             <SelectObbSheet obbSheets={obbSheets} route="/points/gmt-production-back-qc"/>
         </div>
     )
